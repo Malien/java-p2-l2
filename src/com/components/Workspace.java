fde1298bf5cd80413e3c0d0158caea0c8f5d389a
Yaroslav Petryk <yaroslav.petryk@ukma.edu.ua>
2019-02-25 17:40:43 +0200

    Proposing concurrency/multithreading app model based on putting requests in queues with ability to be redirected later
    Created experimental branch for this one

---
+++ src/com/components/Workspace.java
@@ -0,0 +1,13 @@
+package com.components;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Workspace extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        DatabaseManager manager = DatabaseManager.getInstance();
+        //TODO: set up application
+    }
+}
---
+++ src/com/requests/IRequestResponder.java
@@ -0,0 +1,7 @@
+package com.requests;
+
+public interface IRequestResponder {
+
+    void respond(IRequest request);
+
+}
