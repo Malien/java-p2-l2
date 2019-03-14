63a547387409a515704f2cbfa01a3cbff8e0c199
Yaroslav Petryk <yaroslav.petryk@ukma.edu.ua>
2019-03-14 20:41:09 +0200

    A bit of Requests documentation

--- src/com/requests/IRequest.java
+++ src/com/requests/IRequest.java
@@ -2,6 +2,22 @@ package com.requests;
 
 public interface IRequest {
 
+    /* Request-based concurrency model developer guide
+
+    UI event    Request1        Database      Request2         UI
+    handler  ----------------->  manager ------------------> updater
+                           (IRequestHandler)            (IRequestHandler)
+
+                Request1                     Request2
+               type: get                    type: update
+               path: path to entry          path: element id/name
+            payload: null                payload: entry from db
+               next: Request2               next: null
+           next res: UI updater         next res: null
+
+                     Looks like HTTP, doesn't it?
+     */
+
     IRequest nextRequest();
     IRequestResponder nextResponder();
     String path();
