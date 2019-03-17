332acff261a443a0d4c06a514a71a62d05374397
Yaroslav Petryk <yaroslav.petryk@ukma.edu.ua>
2019-03-17 14:21:32 +0200

    Cleanup

--- src/com/components/Workspace.java
+++ src/com/components/Workspace.java
@@ -1,7 +1,5 @@
 package com.components;
 
-import com.data.Product;
-import com.requests.DatabaseRequest;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -11,13 +9,5 @@ public class Workspace extends Application {
     public void start(Stage primaryStage) throws Exception {
         DatabaseManager manager = DatabaseManager.getInstance();
         //TODO: set up application
-        DatabaseRequest req1 = DatabaseRequest.add(null, "/somewhere");
-        DatabaseRequest req2 = DatabaseRequest.get("/whatever", req1, manager);
-        DatabaseRequest req3 = DatabaseRequest.set(
-                new Product("","","", 3, 2),
-                "/whatever/eee"
-        );
-        manager.respond(req2);
-        manager.respond(req3);
     }
 }
