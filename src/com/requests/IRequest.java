30e2d9031639f5730d709fdd4f30df421d13cd11
Yaroslav Petryk <yaroslav.petryk@ukma.edu.ua>
2019-03-20 16:00:03 +0200

    A bit of restructuring. Franchize reboot

--- src/com/requests/IRequest.java
+++ src/com/requests/IRequest.java
@@ -3,12 +3,13 @@ package com.requests;
 /**
  * Interface for async-like queue object
  * @param <T> type of parameter to process
+ * @param <R> type of callback parameter
  */
-public interface IRequest<T> {
+public interface IRequest<T, R> {
 
     String path();
     T payload();
     byte type();
-    Callback callback();
+    Callback<R> callback();
 
 }
