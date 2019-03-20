be7e9fba30d813709ef6e59ac92bd6c5e65b84cb
Yaroslav Petryk <yaroslav.petryk@ukma.edu.ua>
2019-03-20 11:24:09 +0200

    Transfer of basic data classes to master

---
+++ src/com/components/Workspace.java
@@ -0,0 +1,9 @@
+package com.components;
+
+public class Workspace{
+
+    public static void main(String[] args){
+
+    }
+
+}
---
+++ src/com/data/Product.java
@@ -0,0 +1,69 @@
+package com.data;
+
+public class Product {
+
+    private String name;
+    private String description;
+    private String manufacturer;
+    private int count;
+    private double price;
+
+    public Product(String name, String description, String manufacturer, int count, double price) {
+        this.name = name;
+        this.description = description;
+        this.manufacturer = manufacturer;
+        this.count = count;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", manufacturer='" + manufacturer + '\'' +
+                ", count=" + count +
+                ", price=" + price +
+                '}';
+    }
+}
